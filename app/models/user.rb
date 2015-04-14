9a24ede8242eb0ff22cd4cdffd83635856f5a800
Christian Dalsvaag <christian@ddev.no>
2015-04-14 14:53:18 +0200

    Added error messages for form validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,24 +6,27 @@ class User < ActiveRecord::Base
 
   before_save :encrypt_password
 
-  validates :username, length: { in: 1..20, message: "must be between 1 and 15 characters" }
-  validates :username, uniqueness: true
-  validates_format_of :username, :with => /\A\w+\z/i, :message => "can only contain letters and numbers"
+  validates :username, length: { in: 1..20, message: I18n.t('validation.username_length') }
+  validates :username, uniqueness: true, uniqueness: { message: I18n.t('validation.username_exists') }
+  validates_format_of :username, :with => /\A\w+\z/i, message: I18n.t('validation.username_alphanumeric')
 
-  validates :display_name, length: { in: 1..25, message: "must be between 4 and 25 characters" }, if: :display_name_entered
+  validates :display_name, length: { in: 1..25, message: "must be between 1 and 25 characters" }, if: :display_name_entered
   validates_format_of :display_name, :with => /\A[a-z0-9]+ *[a-z0-9]*\z/i, :message => "can only contain letters, spaces, and numbers", if: :display_name_entered
   
   # This is for other actions than on: :create 
   # TODO: This might be redundant
-  validates :password, length: { in: 6..20, message: "must be between 6 and 20 characters" }, if: :password_entered
-  validates :password, presence: true, on: :create
+  validates :password, length: { in: 6..20}, if: :password_entered, on: :update
+  validates :password, length: { in: 6..20, message: I18n.t('validation.password_length') }, on: :create
   
   # TODO: Get better regex for email validation
   validates_format_of :email, 
-    :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  validates :email, uniqueness: true
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
+    message: I18n.t('validation.email_format')
+  validates :email, uniqueness: true, uniqueness: { message: I18n.t('validation.email_exists') }
 
-  validates :phone_number, numericality: {message: "can only contain digits"}, uniqueness: true, if: :phone_number_entered
+  validates :phone_number, numericality: {message: I18n.t('validation.phone_number_numerical') }, 
+    uniqueness: { message: I18n.t('validation.phone_number_exists') },
+    if: :phone_number_entered
 
   protected 
 
