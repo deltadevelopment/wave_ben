9b2257e51a9a6b75b78eb00d11e9b57cdfd6c721
Christian Dalsvaag <christian@ddev.no>
2015-05-15 10:46:07 +0200

    Trying to fix heroku build

--- db/migrate/20150514173142_remove_awesome_nested_set_from_drops.rb
+++ db/migrate/20150514173142_remove_awesome_nested_set_from_drops.rb
@@ -1,9 +1,9 @@
 class RemoveAwesomeNestedSetFromDrops < ActiveRecord::Migration
   def change
     change_table :drops do |t|
-      t.remove :parent_id, :integer
-      t.remove :lft, :integer, null: false
-      t.remove :rgt, :integer, null: false
+      t.remove :parent_id
+      t.remove :lft
+      t.remove :rgt
     end
   end
 end
