128d6f145ed687345c6720cce3b791d623f8ef4d
Christian Dalsvaag <christian@ddev.no>
2015-05-20 13:58:06 +0200

    Fixed issue where media_url was not being returned in feed.

--- app/serializers/drop_serializer.rb
+++ app/serializers/drop_serializer.rb
@@ -2,4 +2,8 @@ class DropSerializer < ActiveModel::Serializer
   attributes :id, :media_key, :temperature, :media_type, :thumbnail_key
 
   has_one :user
+
+  def media_url
+    object.generate_download_uri
+  end
 end
