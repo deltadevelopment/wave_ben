b6fd31dea5c266e4fcdb4280448555626ca247df
Christian Dalsvaag <christian@ddev.no>
2015-05-20 13:56:25 +0200

    Fixed issue where thumbnail_key was not being displayed in feed.

--- app/serializers/feed_serializer.rb
+++ app/serializers/feed_serializer.rb
@@ -12,16 +12,6 @@ class FeedSerializer < ActiveModel::Serializer
   
 end
 
-class DropSerializer < ActiveModel::Serializer
-  attributes :id, :temperature, :media_key, :media_url
-
-  has_one :user
-
-  def media_url
-    object.generate_download_uri
-  end
-end
-
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
 end
