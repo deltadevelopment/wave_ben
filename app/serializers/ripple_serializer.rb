8747fae652e2e8597b843d9452f930e91fd86dfa
Christian Dalsvaag <christian@ddev.no>
2015-06-17 17:08:38 +0200

    Added ID to ripple serializer

--- app/serializers/ripple_serializer.rb
+++ app/serializers/ripple_serializer.rb
@@ -1,5 +1,5 @@
 class RippleSerializer < ActiveModel::Serializer
-  attributes :message, :trigger_id, :trigger_type, :triggee_id , :created_at
+  attributes :id, :message, :trigger_id, :trigger_type, :triggee_id , :created_at
 
   has_one :trigger
   has_one :triggee
