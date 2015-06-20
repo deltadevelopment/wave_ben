b12d4a7fd715a42dcce0b0b78b4ed7ee43b4799f
Christian Dalsvaag <christian@ddev.no>
2015-06-20 18:32:39 +0200

    Final part of refactoring #47

--- app/models/ripple.rb
+++ app/models/ripple.rb
@@ -1,7 +1,37 @@
 class Ripple < ActiveRecord::Base
 
+  after_create :notify_user
+
   belongs_to :interaction
 
   belongs_to :user
+
+  def notify_user
+
+
+  end
+
+  private
+
+  def notify_user
+    message = generate_message
+
+    if user.ios?      
+      inner = {
+        "aps": {
+          "alert": message
+        }
+      }
+      padded_message = { APNS_SANDBOX: inner }.to_json
+    else
+      padded_message = message
+    end
+
+    user.notify(padded_message)
+  end
+
+  def generate_message
+    "The message"
+  end
 
 end
