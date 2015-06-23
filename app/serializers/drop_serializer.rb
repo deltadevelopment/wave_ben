6286115bfbcfd2749788aabba333476be1e013b1
Christian Dalsvaag <christian@ddev.no>
2015-06-23 16:11:11 +0200

    Added thumbnail_key to serializer

--- app/serializers/drop_serializer.rb
+++ app/serializers/drop_serializer.rb
@@ -1,5 +1,5 @@
 class DropSerializer < ActiveModel::Serializer
-  attributes :id, :media_key, :temperature, :media_type, :media_url, :thumbnail_url, :bucket_id
+  attributes :id, :media_key, :temperature, :media_type, :media_url, :thumbnail_key, :thumbnail_url, :bucket_id
 
   has_one :user
 
