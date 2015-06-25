5ea7161608485060b2966d18ae69875c55b6ff80
Christian Dalsvaag <christian@ddev.no>
2015-06-25 19:04:03 +0200

    Added taggable to tag

--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,6 +1,8 @@
 class TagSerializer < ActiveModel::Serializer
   attributes :id, :taggee_id, :taggee_type, :taggable_id, :taggable_type, :user, :taggee
 
+  has_one :taggable
+
   def user
     return UserSerializer.new(object.taggable.user, root: false)
     nil
@@ -23,4 +25,14 @@ class TagSerializer < ActiveModel::Serializer
     Drop.where(bucket_id: object.id).order(created_at: :desc)
   end
 
+end
+
+class BucketSerializer < ActiveModel::Serializer
+  attributes :id, :title
+
+  has_one :drop
+
+  def drop
+    Drop.where(bucket_id: object.id).order(created_at: :desc).first
+  end
 end
