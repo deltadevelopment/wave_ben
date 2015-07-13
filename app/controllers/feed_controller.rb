478a3f3f2f2bd402c7b9ba3d097922e276a466ad
Christian Dalsvaag <christian@ddev.no>
2015-07-13 14:39:45 +0200

    Fixed issue

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -7,10 +7,12 @@ class FeedController < ApplicationController
     subscription_ids = [current_user.id]
     subscriptions.each { |f| subscription_ids.push(f.subscribee_id) }
 
-    public_buckets = 
-      Bucket.public_bucket.with_user_ids(subscription_ids).with_drops
+    # public_buckets = 
+    #   Bucket.public_bucket.with_user_ids(subscription_ids).with_drops
 
-    buckets = Bucket.subscribees(current_user).public_bucket.with_drops.order(updated_at: :desc)
+    # buckets = Bucket.subscribees(current_user).public_bucket.with_drops.order(updated_at: :desc)
+    
+    buckets = Bucket.where("user_id IN (?) AND drops_count > 0", subscription_ids).order(updated_at: :desc)
 
     json_response 200,
       success: true,
